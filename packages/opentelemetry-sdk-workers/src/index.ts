64cda5bcbbf775af33afb1ee869be3d176ee51c7
Richard Simpson <richardsimpson@outlook.com>
2023-03-09 19:14:01 -0600

    feat: start work on a context-based sdk

--- packages/opentelemetry-sdk-workers/src/index.ts
+++ packages/opentelemetry-sdk-workers/src/index.ts
@@ -1,3 +1,4 @@
+import "./performance";
 export * as api from '@opentelemetry/api';
 export * as core from '@opentelemetry/core';
 export * as tracing from '@opentelemetry/sdk-trace-base';
--- packages/opentelemetry-sdk-workers/src/performance.ts
+++ packages/opentelemetry-sdk-workers/src/performance.ts
@@ -1,35 +1,37 @@
-const unsupportedMethods: Array<string | symbol> = [
-	"mark",
-	"clearMarks",
-	"measure",
-	"clearMeasures",
-	"getEntries",
-	"getEntriesByType",
-	"getEntriesByName"
-];
+if (typeof globalThis["performance"] === "undefined") {
+	const unsupportedMethods: Array<string | symbol> = [
+		"mark",
+		"clearMarks",
+		"measure",
+		"clearMeasures",
+		"getEntries",
+		"getEntriesByType",
+		"getEntriesByName"
+	];
 
-class UnsupportedMethodError extends Error {
-	constructor(public method: string) {
-		super(`The method '${method}' is not supported by this polyfill.`);
-		this.name = "UnsupportedMethodError";
-	}
-}
-
-const performanceShim = new Proxy(
-	{
-		now: () => Date.now(),
-		// TODO: How should this behave in a worker?
-		timeOrigin: Date.now()
-	},
-	{
-		get(target, prop, receiver) {
-			if (unsupportedMethods.includes(prop)) {
-				throw new UnsupportedMethodError(prop.toString());
-			}
-			return Reflect.get(target, prop, receiver);
+	class UnsupportedMethodError extends Error {
+		constructor(public method: string) {
+			super(`The method '${method}' is not supported by this polyfill.`);
+			this.name = "UnsupportedMethodError";
 		}
 	}
-);
 
-// @ts-ignore
-globalThis["performance"] = performanceShim;
+	const performanceShim = new Proxy(
+		{
+			now: () => Date.now(),
+			// TODO: How should this behave in a worker?
+			timeOrigin: Date.now()
+		},
+		{
+			get(target, prop, receiver) {
+				if (unsupportedMethods.includes(prop)) {
+					throw new UnsupportedMethodError(prop.toString());
+				}
+				return Reflect.get(target, prop, receiver);
+			}
+		}
+	);
+
+	// @ts-ignore
+	globalThis["performance"] = performanceShim;
+}
